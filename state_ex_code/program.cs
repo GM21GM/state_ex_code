226f6665af54952e00b655d46699093439b84dfd
GM21GM <111045891+GM21GM@users.noreply.github.com>
2024-03-25 07:46:45 +0100

    Projektdateien hinzufügen.

---
+++ state_ex_code/program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new StopwatchImpl();
+
+        // Use the stopwatch
+        stopwatch.LeftButtonPressed();
+
+        // Stopwatch state changed, e.g., to 'RunningState'
+
+        State state = stopwatch.GetState();
+        // Perform actions or state transitions based on the state obtained
+
+        stopwatch.RightButtonPressed();
+
+        // Stopwatch state changed, e.g., to 'PausedState'
+
+        state = stopwatch.GetState();
+        // Perform actions or state transitions based on the state obtained
+    }
+}
---
+++ state_ex_code/stopwatch.cs
@@ -0,0 +1,56 @@
+﻿interface Stopwatch
+{
+    void LeftButtonPressed();
+    void RightButtonPressed();
+    State GetState();
+    void SetState(State state);
+}
+interface State
+{
+    void LeftButtonPressed(Stopwatch stopwatch);
+    void RightButtonPressed(Stopwatch stopwatch);
+}
+class RunningState : State
+{
+    public void LeftButtonPressed(Stopwatch stopwatch) { }
+    public void RightButtonPressed(Stopwatch stopwatch) { }
+}
+class ReadyState : State
+{
+    public void LeftButtonPressed(Stopwatch stopwatch) { }
+    public void RightButtonPressed(Stopwatch stopwatch) { }
+}
+class PausedState : State
+{
+    public void LeftButtonPressed(Stopwatch stopwatch) { }
+    public void RightButtonPressed(Stopwatch stopwatch) { }
+}
+class StoppedState : State
+{
+    public void LeftButtonPressed(Stopwatch stopwatch) { }
+    public void RightButtonPressed(Stopwatch stopwatch) { }
+}
+class StopwatchImpl : Stopwatch
+{
+    private State _state;
+    public StopwatchImpl()
+    {
+        _state = new ReadyState();
+    }
+    public void LeftButtonPressed()
+    {
+        _state.LeftButtonPressed(this);
+    }
+    public void RightButtonPressed()
+    {
+        _state.RightButtonPressed(this);
+    }
+    public State GetState()
+    {
+        return _state;
+    }
+    public void SetState(State state)
+    {
+        _state = state;
+    }
+}
